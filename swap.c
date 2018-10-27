43c81ce215973dd8309be52f30601475473106ef
nikhilamin073 <aminnikhil073@gmail.com>
2018-10-28 01:24:03 +0530

    altered swap logic

--- swap.c
+++ swap.c
@@ -1,24 +1,23 @@
 #include<stdio.h>
 
+//Function to swap 2 numbers without using 3rd variable
 void swap(int *x, int *y)
 {
-
-	int temp=*x;
-	*x=*y;
-	*y = temp;
+	*x = *x + *y;
+   	*y = *x - *y;
+   	*x = *x - *y;
 }
+
 void main()
 {
 	int x=10;
 	int y=5;
 
 	printf("\n Values before swapping");
-	printf("\n X: %d\n Y:%d\n", x,y);
- 	 swap(&x, &y);
+	printf("\n X: %d\n Y: %d\n", x,y);
+ 	
+	swap(&x, &y);
 	
 	printf("\n Values before swapping");
-	printf("\n X: %d\n Y:%d\n", x,y);
-
-
-
+	printf("\n X: %d\n Y: %d\n", x,y);
 }
